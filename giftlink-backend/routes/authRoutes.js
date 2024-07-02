23931cd4eb115993162c0fce4e08b39008f83742
attilakosztindev <attilakosztin.dev@gmail.com>
2024-07-01 21:49:30 -0400

    Implement Back-end in Profile Component

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -1,4 +1,4 @@
-cconst express = require('express');
+const express = require('express');
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const connectToDatabase = require('../models/db');
--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -8,6 +8,7 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import DetailsPage from './components/DetailsPage/DetailsPage';
 import SearchPage from './components/SearchPage/SearchPage';
+import Profile from './components/Profile/Profile';
 
 function App() {
   const navigate = useNavigate();
@@ -20,7 +21,8 @@ function App() {
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/product/:productId" element={<DetailsPage/>} />
-          <Route path="/app/search" element={<SearchPage/>} />  
+          <Route path="/app/search" element={<SearchPage/>} />
+          <Route path="/app/profile" element={<Profile/>} />
         </Routes>
         </>
   );
